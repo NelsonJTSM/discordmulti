d105030d3b6e8b29e85b15e7d494cd6a123810b5
Joseph-Kocis <43280544+Joseph-Kocis@users.noreply.github.com>
2018-10-20 19:12:10 -0400

    First Commit!!!!!!!!!
    Jody

--- src/main/java/com/discordmulti/ConnectFour.java
+++ src/main/java/com/discordmulti/ConnectFour.java
@@ -4,18 +4,31 @@ public class ConnectFour extends Game {
 
     public ConnectFour() {
         super("Connect Four");
+
     }
 
     public void start() {
         super.start();
 
-
+        output("Welcome to Connect Four!\n");
 
 
     }
 
     public void stop() {
         super.stop();
+
+    }
+
+    public void input(Player player, String message) {
+
+        super.input(player, message);
+
+
+
+    }
+
+    public boolean joinPlayer(String id, String name) {
 
     }
 
@@ -90,8 +103,34 @@ class Board {
         return output;
     }
 
-    //public boolean makePlay(int column, boardSpots) {
+    public boolean makePlay(int column, boardSpots player) {
 
-    //}
+        //if the column is not in range
+        if (!(column > 0 && column <= columns)) {
+            //column is not in range
+            return false;
+        }
+
+        //if the player is not Black or Red
+        if (!(player == boardSpots.Black || player == boardSpots.Red)) {
+            //wrong player
+            return false;
+        }
+
+        //the column is full
+        if (board[rows - 1][column] != boardSpots.Empty) {
+            //column is full
+            return false;
+        }
+
+        //the column and player are both valid
+        //make the play
+        for (int i = 0; i < rows; i++) {
+            if (board[i][column] == boardSpots.Empty) {
+                board[i][column] = player;
+            }
+        }
+
+    }
 
 }
--- src/main/java/com/discordmulti/Game.java
+++ src/main/java/com/discordmulti/Game.java
@@ -7,12 +7,45 @@ public class Game {
 
     protected String name;
 
-    public Game(String name) {
+    int minPlayers;
+    int maxPlayers;
+
+    ArrayList<Player> players = new ArrayList<Player>();
+    HashMap<Long, Player> playerHashMap = new HashMap<Long, Player>();
+
+    public Game(String name, int minPlayers, int maxPlayers) {
         this.name = name;
+        this.minPlayers = minPlayers;
+        this.maxPlayers = maxPlayers;
     }
 
     protected void input(Player player, String message) {
+
+        //handles people joining the game
+        if (message.toLowerCase().equals("join")) {
+            if (addPlayer(player.getId(), player.getName())) {
+                output(player.getName() + " has successfully joined the game");
+            } else {
+                output(player.getName() + " was already in the game");
+            }
+        }
+
         System.out.println(player.getName() + ":\"" + message + "\"");
+    }
+
+    protected boolean addPlayer(long id, String name) {
+
+        //if the player isn't already added
+        if (!playerHashMap.containsKey(id)) {
+            Player player = new Player(name, id);
+            players.add(player);
+            playerHashMap.put(id, player);
+
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     // TODO: Change for discord
